1a8eabe6d53cee8bd0f0c2f201a65b08bd71734c
Rae <rachaelwilkens@gmail.com>
2025-12-07 13:01:15 -0600

    F1

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,53 +1,69 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
 
 const Posts = () => {
+    let navigate = useNavigate();
   const { id } = useParams();
   const [posts, setPosts] = useState([]);
-  const [loading, setLoading] = useState[true];
+  const [loading, setLoading] = useState();
+  const [searchId, setSearchId] = useState(id);
 
-function onSearch() {
-    
+  function onSearch() {
+    fetchPosts(searchId);
+  }
+
+  async function fetchPosts(userId) {
+    setLoading(true);
+    const { data } = await axios.get(
+      `https://jsonplaceholder.typicode.com/posts?userID=${userId || id}`
+    );
+    setPosts(data);
+    setLoading(false);
+  }
+
+  function onSearchKeyPress(key) {
+    if (key === 'Enter') {
+        onSearch()
+    }
 }
 
   useEffect(() => {
-    async function fetchPosts() {
-      const { data } = await axios.get(
-        `https://jsonplaceholder.typicode.com/posts?userID=${id}`
-      );
-      setPosts(data);
-      setLoading(false);
-    }
     fetchPosts();
   }, []);
+
   return (
     <>
       <div className="post__search">
-        <button>← Back</button>
+        <button onClick={() => navigate('/')}>← Back</button>
         <div className="post__search--container">
           <label className="post__search--label">Search by Id</label>
-          <input type="number" />
-          <button onClick={() => onSerch()}>Enter</button>
+          <input
+            type="number"
+            value={searchId}
+            onChange={(event) => setSearchId(event.target.value)}
+            onKeyPress={(event) => onSearchKeyPress(event.key)}
+          />
+          <button onClick={() => onSearch()}>Enter</button>
         </div>
       </div>
-      {loading 
-      ? new Array(10).fill(0).map((_, index) => (
-        <div className="post" key={index}>
-        <div className="post__title">
-          <div className="post__title--skeleton"></div>
-        </div>
-        <div className="post__body">
-          <p className="post__body--skeleton"></p>
-        </div>
-      </div>
-      )) 
-      : posts.map(post => (
-        <div className="post" key={post.id}>
-          <div className="post__title">{post.title}</div>
-          <p className="post__body">{post.body}</p>
-        </div>
-    ))} 
+      {loading
+        ? new Array(10).fill(0).map((_, index) => (
+            <div className="post" key={index}>
+              <div className="post__title">
+                <div className="post__title--skeleton"></div>
+              </div>
+              <div className="post__body">
+                <p className="post__body--skeleton"></p>
+              </div>
+            </div>
+          ))
+        : posts.map((post) => (
+            <div className="post" key={post.id}>
+              <div className="post__title">{post.title}</div>
+              <p className="post__body">{post.body}</p>
+            </div>
+          ))}
     </>
   );
 };
